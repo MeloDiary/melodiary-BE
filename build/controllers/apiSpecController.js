c2f14e101af6dfca49c17ede1a318f5cfff03148
dukkov <dukov.ko.dev@gmail.com>
2024-08-03 00:37:39 +0900

    [Feat] 구글 회원가입 기능 구현 #10 - 구글 회원가입 기능 구현 완료 - API 명세서 수정 - JWT 발급 모듈 작성 - DB 연결 설정

--- build/controllers/apiSpecController.js
+++
@@ -1,8 +0,0 @@
-import YAML from 'yamljs';
-import swaggerui from 'swagger-ui-express';
-import path from 'path';
-import { getDirname } from '../utils/getDirname.js';
-const __dirname = getDirname(import.meta.url);
-const apiSpecDoc = YAML.load(path.join(__dirname, '../../docs/API_spec.yaml'));
-export const serveSwaggerUi = swaggerui.serve;
-export const setupSwaggerUi = swaggerui.setup(apiSpecDoc);
--- build/utils/getDirname.js
+++
@@ -1,6 +0,0 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-export function getDirname(metaUrl) {
-    const __filename = fileURLToPath(metaUrl);
-    return dirname(__filename);
-}
